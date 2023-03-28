cf9019b557d8884f0adee880f73eb12cfb4f24f4
AleksandrMinyshk <alexmech001@gmail.com>
2023-03-28 18:38:07 -0400

    V.1

---
+++ src/HomeWork/HW_3_26_2023.java
@@ -0,0 +1,17 @@
+package HomeWork;
+
+public class HW_3_26_2023 {
+    public static void main(String[] args) {
+        //  Create a repo on local, have a basic code in it for printing integers from 1 to 10
+        //  Push it to github.
+
+        //  Step2
+        //  Clone the pushed repo as being a member of a project.
+        //  Update the code and make it print integers till 100.
+        //  Push the code to the same repo
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args){
 
-        for(int i=0;i<10;i++){
-        System.out.println(i);
+        for(int i=0;i<5;i++){
+            System.out.println(i);
         }
     }
 }
